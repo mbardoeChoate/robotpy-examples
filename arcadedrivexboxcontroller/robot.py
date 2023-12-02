7b13153e03be869d91cac1b1928d964921cbc45e
Matthew Bardoe <mbardoe@gmail.com>
2023-12-02 18:39:47 -0500

    comments in greater alignment with the comments in allwpilib examples

--- arcadedrivexboxcontroller/robot.py
+++ arcadedrivexboxcontroller/robot.py
@@ -1,39 +1,45 @@
 #!/usr/bin/env python3
-"""
-    This is a demo program showing the use of the DifferentialDrive class,
-    specifically it contains the code necessary to operate a robot with
-    split arcade drive with a single Xbox controller
-"""
+
 
 import wpilib
 import wpilib.drive
 
 
 class MyRobot(wpilib.TimedRobot):
+    """
+    This is a demo program showing the use of the DifferentialDrive class.
+    Runs the motors with split arcade steering and an Xbox controller.
+    """
+
     def robotInit(self):
-        """Robot initialization function"""
+        self.left = wpilib.Talon(0)
+        self.right = wpilib.Talon(1)
 
-        # create motor controller objects
-        m_left = wpilib.Talon(0)
-        m_right = wpilib.Talon(1)
+        ##
+        # We need to invert one side of the drivetrain so that positive voltages
+        # result in both sides moving forward. Depending on how your robot's
+        # gearbox is constructed, you might have to invert the left side instead."""
+        ##
 
-        # invert the right side motors
-        m_right.setInverted(True)
+        self.right.setInverted(True)
 
-        # object that handles basic drive operations
-        self.myRobot = wpilib.drive.DifferentialDrive(m_left, m_right)
-        self.myRobot.setExpiration(0.1)
+        self.robotDrive = wpilib.drive.DifferentialDrive(self.left, self.right)
+        self.robotDrive.setExpiration(0.1)
 
-        # X box controller
         self.controller = wpilib.XboxController(0)
 
     def teleopInit(self):
         """Executed at the start of teleop mode"""
-        self.myRobot.setSafetyEnabled(True)
+        self.robotDrive.setSafetyEnabled(True)
 
     def teleopPeriodic(self):
-        """Runs the motors with tank steering"""
-        self.myRobot.arcadeDrive(
+        ##
+        # Drive with split arcade drive.
+        # That means that the Y axis of the left stick moves forward
+        # and backward, and the X of the right stick turns left and right.
+        ##
+
+        self.robotDrive.arcadeDrive(
             -self.controller.getLeftY(), -self.controller.getRightY()
         )
 
