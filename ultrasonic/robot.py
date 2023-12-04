dfb4e16f30a05829c963bbe5008f41e351cb0acd
Matthew Bardoe <mbardoe@gmail.com>
2023-12-03 20:08:51 -0500

    Changed ultrasonic range method to those supported in robotpy

--- ultrasonic/robot.py
+++ ultrasonic/robot.py
@@ -12,41 +12,30 @@ class MyRobot(wpilib.TimedRobot):
     """
 
     def robotInit(self):
-        ##
         # Creates a ping-response Ultrasonic object on DIO 1 and 2.
-        ##
         self.rangeFinder = wpilib.Ultrasonic(1, 2)
 
-        ##
         # Add the ultrasonic to the "Sensors" tab of the dashboard
         # Data will update automatically
-        ##
-
         Shuffleboard.getTab("Sensors").add(self.rangeFinder)
 
     def teleopPeriodic(self):
         # We can read the distance in millimeters
-        distanceMillimeters = self.rangeFinder.getRangeMM()
-        # ... or in inches
-        distanceInches = self.rangeFinder.getRangeInches()
+        distanceMeters = self.rangeFinder.getRange()
 
         # We can also publish the data itself periodically
-        SmartDashboard.putNumber("Distance[mm]", distanceMillimeters)
-        SmartDashboard.putNumber("Distance[in]", distanceInches)
+        SmartDashboard.putNumber("Distance[m]", distanceMeters)
 
     def testInit(self):
-        ##
         # By default, the Ultrasonic class polls all ultrasonic sensors every in a round-robin to prevent
         # them from interfering from one another.
         # However, manual polling is also possible -- notes that this disables automatic mode!
-        ##
         self.rangeFinder.ping()
 
     def testPeriodic(self):
         if self.rangeFinder.isRangeValid():
             # Data is valid, publish it
-            SmartDashboard.putNumber("Distance[mm]", self.rangeFinder.getRangeMM())
-            SmartDashboard.putNumber("Distance[in]", self.rangeFinder.getRangeInches())
+            SmartDashboard.putNumber("Distance[m]", self.rangeFinder.getRange())
 
             # Ping for next measurement
             self.rangeFinder.ping()
